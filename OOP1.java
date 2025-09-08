e67f043a5d52e7f9f056fd5f74aea4fe7bc41b2b
Ananthakrishnan <ak2472006@gmail.com>
2025-09-08 09:51:45 +0530

    Create OOP1.java

---
+++ OOP1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.time.format.DateTimeFormatter;
+
+class Account{
+    private int AccountId;
+    private String Name;
+    private int phoneNumber;
+    private String email;
+    private String address;
+
+    public Account(int AccountId,String Name,int phoneNumber,String email,String address){
+        this.AccountId=AccountId;
+        this.Name=Name;
+        this.phoneNumber=phoneNumber;
+        this.email=email;
+        this.address=address;
+    }
+}
+
+public class OOP1 {
+
+}
