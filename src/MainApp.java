b7882f6946603511e4268a1ce72f79217e3cc770
abhiram-b <abhiramb736@gmail.com>
2025-09-21 00:24:35 +0530

    created a folder structure

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,2 +1,41 @@
+
+
+import com.garbage.model.User;
+import com.garbage.model.Report;
+import com.garbage.util.MongoDBUtil;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class MainApp {
+    public static void main(String[] args) {
+        // Connect to DB
+        MongoDatabase db = MongoDBUtil.getDatabase();
+        MongoCollection<Document> usersCollection = db.getCollection("Users");
+        MongoCollection<Document> reportsCollection = db.getCollection("Reports");
+
+        // Create a sample User
+        User user = new User("Alice", "9876543210", "alice@example.com", "hashed_password");
+        Document userDoc = new Document("_id", user.getId())
+                .append("name", user.getName())
+                .append("phoneNumber", user.getPhoneNumber())
+                .append("email", user.getEmail())
+                .append("passwordHash", user.getPasswordHash())
+                .append("dateRegistered", user.getDateRegistered());
+        usersCollection.insertOne(userDoc);
+        System.out.println("Inserted User: " + user.getName());
+
+        // Create a Report linked to the User
+        Report report = new Report(user.getId(), "Plastic", 5.5, "Central Park", "Plastic bottles near the lake");
+        Document reportDoc = new Document("_id", report.getId())
+                .append("userId", report.getUserId())
+                .append("wasteType", report.getWasteType())
+                .append("quantity", report.getQuantity())
+                .append("location", report.getLocation())
+                .append("status", report.getStatus())
+                .append("dateSubmitted", report.getDateSubmitted())
+                .append("description", report.getDescription());
+        reportsCollection.insertOne(reportDoc);
+        System.out.println("Inserted Report at: " + report.getLocation());
+    }
 }
--- src/utils/MongoDBUtil.java
+++ src/utils/MongoDBUtil.java
@@ -1,4 +1,18 @@
 package utils;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 public class MongoDBUtil {
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final String DB_NAME = "GarbageDB";
+    private static MongoClient mongoClient = null;
+
+    public static MongoDatabase getDatabase() {
+        if (mongoClient == null) {
+            mongoClient = MongoClients.create(CONNECTION_STRING);
+        }
+        return mongoClient.getDatabase(DB_NAME);
+    }
 }
